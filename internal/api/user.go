2615114e834fc2456b16bbf5b0c762a7aea032af
Meysam Bavi <meybsky@gmail.com>
2022-09-14 01:16:39 +0430

    Add user api
    Add create and login
    Add user dto
    Add validation

---
+++ internal/api/user.go
@@ -0,0 +1,101 @@
+package api
+
+import (
+	"errors"
+	"net/http"
+
+	"github.com/MeysamBavi/http-monitoring/internal/auth"
+	"github.com/MeysamBavi/http-monitoring/internal/model"
+	"github.com/MeysamBavi/http-monitoring/internal/request"
+	"github.com/MeysamBavi/http-monitoring/internal/store"
+	"github.com/labstack/echo/v4"
+	"go.uber.org/zap"
+)
+
+type UserHandler struct {
+	Logger     *zap.Logger
+	UserStore  store.User
+	JwtHandler *auth.JwtHandler
+}
+
+func (h *UserHandler) Register(group *echo.Group) {
+	group.POST("/create", h.create)
+	group.POST("/login", h.login)
+}
+
+func (h *UserHandler) create(c echo.Context) error {
+
+	var req request.User
+
+	if err := c.Bind(&req); err != nil {
+		h.Logger.Error("error binding request", zap.Error(err))
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+
+	if err := req.Validate(); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+
+	ctx := c.Request().Context()
+	user := model.User{
+		Username: req.Username,
+		Password: req.Password,
+	}
+
+	err := h.UserStore.Add(ctx, &user)
+
+	if err != nil {
+		var duplicate store.DuplicateError
+		if errors.As(err, &duplicate) {
+			h.Logger.Error("duplicate username", zap.Error(duplicate))
+			return echo.NewHTTPError(http.StatusBadRequest, "this username is already taken")
+		}
+
+		return echo.ErrInternalServerError
+	}
+
+	h.Logger.Info("user created", zap.Any("user", user))
+
+	return c.JSON(http.StatusCreated, user)
+}
+
+func (h *UserHandler) login(c echo.Context) error {
+	var req request.User
+
+	if err := c.Bind(&req); err != nil {
+		h.Logger.Error("error binding request", zap.Error(err))
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+
+	if err := req.Validate(); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+
+	ctx := c.Request().Context()
+	user, err := h.UserStore.GetByUsername(ctx, req.Username)
+
+	if err != nil {
+		var notFound store.NotFoundError
+		if errors.As(err, &notFound) {
+			h.Logger.Error("user not found", zap.Error(notFound))
+			return echo.NewHTTPError(http.StatusNotFound, "user not found")
+		}
+
+		return echo.ErrInternalServerError
+	}
+
+	if user.Password != req.Password {
+		return echo.NewHTTPError(http.StatusUnauthorized, "invalid password")
+	}
+
+	h.Logger.Info("user logged in", zap.Any("user", user))
+
+	token, err := h.JwtHandler.GenerateFromUser(user)
+
+	if err != nil {
+		h.Logger.Error("error generating token", zap.Error(err))
+		return echo.ErrInternalServerError
+	}
+
+	return c.JSON(http.StatusOK, token)
+}
--- internal/cmd/serve/main.go
+++ internal/cmd/serve/main.go
@@ -1,32 +1,51 @@
 package serve
 
 import (
+	"github.com/MeysamBavi/http-monitoring/internal/api"
+	"github.com/MeysamBavi/http-monitoring/internal/auth"
 	"github.com/MeysamBavi/http-monitoring/internal/config"
+	"github.com/MeysamBavi/http-monitoring/internal/store"
 	"github.com/labstack/echo/v4"
 	"github.com/spf13/cobra"
 	"go.uber.org/zap"
 )
 
 func main(cfg *config.Config, logger *zap.Logger) {
-	//Todo
-
 	app := echo.New()
-	app.GET("/", func(c echo.Context) error {
-		return c.String(200, "Hello, World!")
-	})
+
+	var s store.Store
+	{
+		logger := logger.Named("store")
+		s = store.NewInMemoryStore(logger.Named("memory"))
+	}
+
+	var jh *auth.JwtHandler
+	{
+		jh = auth.NewJwtHandler(cfg.Auth)
+	}
+
+	{
+		logger := logger.Named("endpoint")
+
+		uh := api.UserHandler{
+			Logger:     logger.Named("user"),
+			UserStore:  s.User(),
+			JwtHandler: jh,
+		}
+		uh.Register(app.Group("/user"))
+	}
 
 	app.Debug = cfg.Debug
 
 	app.Start(cfg.Listen)
 }
 
-
 func New(cfg *config.Config, logger *zap.Logger) *cobra.Command {
 	return &cobra.Command{
-		Use: "serve",
+		Use:   "serve",
 		Short: "Runs the http server",
 		Run: func(cmd *cobra.Command, args []string) {
-			main(cfg, logger);
+			main(cfg, logger)
 		},
 	}
 }
